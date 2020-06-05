d3cd9045c3a42845dcba87bcddf1070a55f31b75
Patrick Cremin <patrickfxcremin@gmail.com>
2020-06-05 01:44:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,10 @@
-base_hash = {
-  :railroads => {
-    "monopoly_with_second_tier" =>
-    :pieces => 4, 
-    :rent_in_dollars => {}
-    :names => {}
-  }
-  }
+def base_hash
+monopoly = {
+  :railroads => {}
+}  
 end
 
- 
-    
-end
+
 
 def monopoly_with_third_tier
 
